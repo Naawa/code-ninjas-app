37f3324ea3a19e933660bf31ff76b8981086595a
Naous Islam <naous.islam@outlook.com>
2024-06-20 12:30:28 -0700

    first commit

---
+++ src/lib/components/landing/LoginModal.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    let showModal = false;
+</script>
+
+<button on:click={() => { showModal = true }}>
+    Login
+</button>
+
+{#if showModal}
+<section>
+    <div>
+        <button on:click={() => {
+            showModal = false
+        }}>Exit</button>
+        <a href="/admin/login"><button>Admin</button></a>
+        <a href="/student/login"><button>Student</button></a>
+    </div>
+</section>
+{/if}
+
+<style lang="scss">
+    section {
+        position: absolute;
+        height: 100vh;
+        width: 100vw;
+        top: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        div {
+            padding: 2em;
+            background-color: white;
+        }
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import LoginModal from "$lib/components/landing/LoginModal.svelte";
+</script>
+
+<h1>Home Page</h1>
+
+
+<LoginModal></LoginModal>
