463a35ea111a6dd5352772ea043b4ffdb9424aa1
rose.rabano <cesseurabano@gmail.com>
2024-06-21 14:09:03 -0700

    Login Front-End Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,15 @@
 	import LoginModal from "$lib/components/landing/LoginModal.svelte";
 </script>
 
-<h1>Home Page</h1>
-<h2>Testing </h2>
 
-<LoginModal></LoginModal>
+<h1>Home Page</h1>
+<div id="login-section">
+	
+<h2>Testing </h2>
+</div>
+
+
+<LoginModal>
+
+	
+</LoginModal>
